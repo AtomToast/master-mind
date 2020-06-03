b479cff07aa333ed890cccb79a2688f99c38afeb
AtomToast <reg-2@t-online.de>
2020-06-03 11:18:14 +0200

    solve exercise 2

--- mastermind.py
+++ mastermind.py
@@ -7,7 +7,8 @@ code = []
 for x in range(0, 4):
     code.append(randint(1, 6))
 
-print("There is a hidden code of 4 digits(1-6). Try to guess it!\nI will tell you how many digits you got right. You need to solve the code in 12 tries or you lose!")
+print("There is a hidden code of 4 digits(1-6). Try to guess it!\n\
+I will tell you how many digits you got right. You need to solve the code in 12 tries or you lose!")
 
 attempts_left = 12
 
@@ -26,16 +27,22 @@ while attempts_left > 0:
         continue
 
     correct_guesses = 0
+    wrong_position = 0
 
-    for i, v in enumerate(guess):
+    for i, v in enumerate(guess_list):
         if code[i] == v:
             correct_guesses += 1
+        elif v in code:
+            wrong_position += 1
 
     if correct_guesses == 4:
-        print(f"You guessed the code with {attempts_left} attempts left")
-        break
+        print(f"You guessed the code with {attempts_left} attempts left!")
+        exit()
 
-    print(f"You guessed {correct_guesses} digits correctly!")
+    print(f"You guessed {correct_guesses} digits correctly! \
+          {wrong_position} digits were the correct number but in the wrong position")
+    print(code)
+
     attempts_left -= 1
 
 print(f"Oops! You're out of attempts! The correct code was {code}")
